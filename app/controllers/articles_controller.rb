364f9f932ed52b1a8668a40ac85dbd36578d6903
faisal <faisalshaikh8433@gmail.com>
2019-04-04 19:27:34 +0530

    progress in blog app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,56 @@
 class ArticlesController < ApplicationController
+	
+	def index
+		@articles = Article.all
+	end
+
 	def new
+		@article = Article.new
 	end
 
 	def create
-		render plain: params.inspect
+		# render plain: params[:article]["title"].inspect
+		@article = Article.new(article_params)
+
+		if @article.save
+			redirect_to @article
+     	else
+     		render 'new'
+     	end	
+
+		# render plain: params[:article].inspect
+
 	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def edit
+	  @article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			return 'edit'
+		end
+	end
+
+	
+	def destroy
+	  @article = Article.find(params[:id])
+	  @article.destroy
+	 
+	  redirect_to articles_path
+	end
+
+
+	private
+	  	def article_params
+	    params.require(:article).permit(:title, :body)
+	  	end
+
 end
